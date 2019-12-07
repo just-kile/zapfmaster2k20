fa91dcc8b8af1581d8985b5c5db6e09b6e82621c
Ben Lenser <ben.lenser@immobilienscout24.de>
2019-12-07 22:55:09 +0100

    Add drops.png to overview

--- lib/ui/screens/overview.dart
+++ lib/ui/screens/overview.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:zapfmaster2k20/core/constants/app_contstants.dart';
 import 'package:zapfmaster2k20/ui/components/best_list/best_list.dart';
 import 'package:zapfmaster2k20/ui/shared/ui_helpers.dart';
 
@@ -7,27 +6,26 @@ class Overview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 241, 159),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            UIHelper.verticalSpaceLarge,
-            UIHelper.verticalSpaceMedium,
-            BestList()
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/drops.png"),
+//            fit: BoxFit.fitWidth,
+            repeat: ImageRepeat.repeat,
+          ),
         ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              UIHelper.verticalSpaceLarge,
+              UIHelper.verticalSpaceMedium,
+              BestList()
+            ],
+          ),
+        ) /* add child content here */,
       ),
-    );
-  }
-
-  RaisedButton buildRaisedButton(BuildContext context) {
-    return RaisedButton(
-      child: Text('Open routeee'),
-      onPressed: () {
-        Navigator.pushNamed(context, RoutePaths.Details);
-      },
     );
   }
 }
